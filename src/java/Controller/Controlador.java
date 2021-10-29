bb0b26673ed1bb1480304d0ec424fd9ad3f29069
Kenyi Hancco <kenyihq@gmail.com>
2021-10-28 21:21:12 -0500

    Addd Controlador

--- src/java/Controller/Controlador.java
+++ src/java/Controller/Controlador.java
@@ -5,10 +5,32 @@
  */
 package Controller;
 
+import Config.Conexion;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+
 /**
  *
  * @author kenyi
  */
+@Controller
 public class Controlador {
+    
+    // Instanciamos conexión
+    Conexion con = new Conexion();
+    JdbcTemplate jdbcTemplate = new JdbcTemplate(con.Conectar());
+    ModelAndView mav = new ModelAndView();
+        
+    @RequestMapping("index.html")
+    public ModelAndView Listar () {
+        String query = "SELECT * FROM nombre";
+        List datos = this.jdbcTemplate.queryForList(query);
+        mav.addObject("lista", datos);
+        mav.setViewName("index");
+        return mav;
+    }
     
 }
